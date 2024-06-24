012dd2b8cd2a84d517ed1936403f0f6c38b4cca6
Monitista <monica.gonzalez.i@estudiantat.upc.edu>
2024-06-25 00:02:40 +0200

    wow

--- Assets/Material/Scripts/Next.cs
+++ Assets/Material/Scripts/Next.cs
@@ -2,27 +2,13 @@ using UnityEngine;
 
 public class Next : MonoBehaviour
 {
-    public GameObject Menu1; // Prefab que se mostrará inicialmente
-    public GameObject Menu2; // Prefab que se mostrará al pulsar el botón
+    public GameObject initialPrefab; // Prefab que se mostrará inicialmente
+    public GameObject mainGamePrefab; // Prefab que se mostrará al pulsar el botón
 
-    private GameObject initialInstance; // Instancia del prefab inicial
-    private GameObject mainGameInstance; // Instancia del prefab principal
-
-    void Start()
+    // Función para cambiar de Prefab
+    public void SwitchPrefabs()
     {
-        // Instanciar los prefabs
-        initialInstance = Instantiate(Menu1);
-        mainGameInstance = Instantiate(Menu2);
-
-        // Asegurarse de que el prefab inicial esté visible y el principal esté oculto al inicio
-        initialInstance.SetActive(true);
-        mainGameInstance.SetActive(false);
-    }
-
-    // Función para cambiar de prefab
-    public void SwitchPrefab()
-    {
-        initialInstance.SetActive(false); // Ocultar el prefab inicial
-        mainGameInstance.SetActive(true); // Mostrar el prefab principal
+        initialPrefab.SetActive(false); // Ocultar el prefab inicial
+        mainGamePrefab.SetActive(true); // Mostrar el prefab principal
     }
 }
